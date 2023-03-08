6efdabc2352cf2e7df091ce11ec487a70d4cfa19
mcbryan1 <mcbryansolomon@yahoo.com>
2023-03-08 15:27:45 -0800

    Mandatory

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * wildcmp -  compares two strings and returns 1 if similar, otherwise 0
+ * @s1: first string
+ * @s2: second string
+ * Return: the integer
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+	{
+		return (1);
+	}
+	if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	if (*s2 == '*')
+	{
+		if (*(s2 + 1) == '\0')
+		{
+			return (1);
+		}
+	}
+	if (*s1 != '\0' && wildcmp(s1 + 1, s1))
+	{
+		return (1);
+	}
+	else
+	{
+		return (wildcmp(s1, s2 + 1));
+	}
+	return (0);
+}
